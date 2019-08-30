ed5280ef4ed115a7856daddf10f4389ebb0bc1fa
yadavsandy1020 <sandeep.yadav03@mphasis.com>
2019-08-30 19:58:00 +0530

    removed creds

--- REST/app.js
+++ REST/app.js
@@ -1,10 +1,10 @@
 const Twitter = require('twitter');
 
 const client = new Twitter({
-  consumer_key: 'MlP41tZZmczyN6Tba6ESzu89W',
-  consumer_secret: 'UzakjyPG0ymiOi1zFkmdYADD6FmkueTL3m9XoeziFtv1kBATd3',
-  access_token_key: '2568133188-F51py9GjktAq0Cf1MKysaJ4oum4XwwsqozhdnKE',
-  access_token_secret: 'lqbE2UKYxvvmKR6kP3b7BjkSjHdQy44t09rxhPAXM1mU7'
+  consumer_key: '',
+  consumer_secret: '',
+  access_token_key: '',
+  access_token_secret: ''
 });
 
  
